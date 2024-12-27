e2c66fb2e250d5eb8a39329987c7325333b4840a
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2024-12-27 13:46:31 -0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
-    prerender: {
-      handleHttpError: ({ path, message }) => {
-        console.error(`${path}: ${message}`);
-      }
-    }
-  }
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',  // This is important for SPA
+      precompress: false,
+      strict: true
+    })
+  },
+  preprocess: vitePreprocess()
 };
 
 export default config;
